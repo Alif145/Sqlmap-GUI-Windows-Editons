372dab4bf19f0518c0a05f5343691939161a0a67
Zerox-Uchiha <105264591+Alif145@users.noreply.github.com>
2025-02-11 02:49:19 +0600

    Add files via upload

---
+++ request/redirecthandler.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""
+$Id: redirecthandler.py 1488 2010-03-16 14:30:57Z inquisb $
+
+This file is part of the sqlmap project, http://sqlmap.sourceforge.net.
+
+Copyright (c) 2007-2010 Bernardo Damele A. G. <bernardo.damele@gmail.com>
+Copyright (c) 2006 Daniele Bellucci <daniele.bellucci@gmail.com>
+
+sqlmap is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation version 2 of the License.
+
+sqlmap is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along
+with sqlmap; if not, write to the Free Software Foundation, Inc., 51
+Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+
+import urllib2
+
+class SmartRedirectHandler(urllib2.HTTPRedirectHandler):
+    def common_http_redirect(self, result, headers, code):
+        if "location" in headers:
+            result.redurl = headers.getheaders("location")[0].split("?")[0]
+        elif "uri" in headers:
+            result.redurl = headers.getheaders("uri")[0].split("?")[0]
+
+        result.redcode = code
+
+        return result
+
+    def http_error_301(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPRedirectHandler.http_error_301(self, req, fp, code, msg, headers)
+        return self.common_http_redirect(result, headers, code)
+
+    def http_error_302(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, headers)
+        return self.common_http_redirect(result, headers, code)
